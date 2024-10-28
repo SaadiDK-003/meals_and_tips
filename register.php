6324151f6c96b4e290b67ec928b8b3b11e5c9813
SaadiDK-003 <pcremote059@gmail.com>
2024-10-29 04:24:03 +0500

    changes

--- register.php
+++ register.php
@@ -17,6 +17,60 @@ if (isLoggedin()) {
 
 <body id="register">
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 mx-auto">
+                <?php
+                if (isset($_POST['email']) && isset($_POST['password'])):
+                    echo register($_POST);
+                endif;
+                ?>
+                <form action="" method="post">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2 class="text-center">Register</h2>
+                        </div>
+                        <div class="col-12 col-md-6 mb-3">
+                            <div class="form-group">
+                                <label for="username">Name</label>
+                                <input type="username" name="username" id="username" required class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-3">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" id="email" required class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-3">
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password" required class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 mb-3">
+                            <div class="form-group">
+                                <label for="role">Role</label>
+                                <select type="role" name="role" id="role" required class="form-select">
+                                    <option value="" selected hidden>Select Role</option>
+                                    <option value="user">User</option>
+                                    <option value="nutritionist">Nutritionist</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12 mb-3">
+                            <div class="form-group d-flex align-items-center justify-content-between">
+                                <a class="nav-link" href="login.php">LOGIN</a>
+                                <button type="submit" name="submit" id="submit" class="btn btn-primary d-block ms-auto">
+                                    REGISTER
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     <?php include 'includes/external_js.php'; ?>
 </body>
