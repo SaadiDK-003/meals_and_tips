edeb5f75ad161d13af9f872991652276090cb368
SaadiDK-003 <pcremote059@gmail.com>
2024-10-29 04:11:39 +0500

    change

--- index.php
+++ index.php
@@ -17,7 +17,27 @@ if (!isLoggedin()) {
 
 <body id="home">
     <?php include_once 'includes/header.php'; ?>
-
+    <section class="hero">
+        <div class="container mx-auto">
+            <div class="row">
+                <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                    <div class="content">
+                        <h1 class="text-white fw-bold"><?= TITLE ?></h1>
+                        <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, nisi illo quam repellendus quo incidunt. At eligendi rem delectus doloremque repellendus porro, totam quam labore alias omnis fuga, repellat quo.</p>
+                        <div class="d-flex align-items-center gap-3 buttons">
+                            <a class="btn btn-primary" href="#!">Recipes</a>
+                            <a class="btn btn-secondary" href="#!">Educational Content</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="meal-plan-img">
+                        <img src="img/Meal-Planning.jpg" alt="meal-plan">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <?php include_once 'includes/footer.php'; ?>
     <?php include 'includes/external_js.php'; ?>
