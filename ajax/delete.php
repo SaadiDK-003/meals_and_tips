e7fe91414beecfe3de296dd9ed8866144d01af23
SaadiDK-003 <pcremote059@gmail.com>
2024-11-09 21:47:32 +0500

    changes

--- ajax/delete.php
+++ ajax/delete.php
@@ -5,3 +5,34 @@ if (isset($_POST['del_id'])):
     $del_id = $_POST['del_id'];
     echo delete('recipes', $del_id);
 endif;
+
+
+if (isset($_POST['fav_id']) && !isset($_POST['del_id'])):
+
+    $fav_id = $_POST['fav_id'];
+    $usrID = $_POST['usrID'];
+
+    $result = $db->query("SELECT `fav_recipes` FROM `users` WHERE `id` = '$usrID'");
+    $cur_val = mysqli_fetch_object($result);
+    $current_value = $cur_val->fav_recipes;
+
+    $fav_array = $current_value ? explode(',', trim($current_value, ',')) : [];
+
+    if (($key = array_search($fav_id, $fav_array)) !== false) {
+
+        unset($fav_array[$key]);
+
+        $new_value = implode(',', $fav_array);
+
+        $upd_fav_Q = $db->query("UPDATE `users` SET `fav_recipes`='$new_value' WHERE `id`='$usrID'");
+        if ($upd_fav_Q) {
+            echo json_encode(["class_" => "text-bg-danger", "msg" => "Deleted Successfully.", "status" => "success"]);
+        } else {
+            echo json_encode(["class_" => "text-bg-danger", "msg" => "Something went wrong.", "status" => "error"]);
+        }
+    } else {
+        echo json_encode(["class_" => "text-bg-danger", "msg" => "ID not found.", "status" => "error"]);
+    }
+
+
+endif;
--- userDashboard.php
+++ userDashboard.php
@@ -38,9 +38,9 @@ if ($userRole == 'nutritionist') {
                             <p class="mb-0 text-center"><?= $list_recipes->instructions ?></p>
                             <div class="buttons_wrapper d-flex w-100 justify-content-center gap-2">
                                 <a href="#!" data-id="<?= $list_recipes->recipe_id ?>" data-bs-toggle="modal" data-bs-target="#RecipeDetails" class="btn-recipe-details btn btn-primary w-75">Details</a>
-                                <!-- <a href="#!" onclick="alert('work in-progress')" data-id="< ?= $list_recipes->recipe_id ?>" class="btn-recipe-fav btn btn-danger w-25">
-                                    <i class="fas fa-star"></i>
-                                </a> -->
+                                <a href="#!" data-id="<?= $list_recipes->recipe_id ?>" data-usr="<?= $userID ?>" class="btn-recipe-fav btn btn-danger w-25">
+                                    <i class="fas fa-trash"></i>
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -54,8 +54,109 @@ if ($userRole == 'nutritionist') {
     </div>
 
 
+    <!-- Modal -->
+    <div class="modal fade" id="RecipeDetails" tabindex="-1" aria-labelledby="RecipeDetailsLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="RecipeDetailsLabel">Recipe Details | <span id="category_name" class="text-success"></span></h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2 id="title" class="text-center fs-2">Details will come here...</h2>
+                            <div class="ingredients-wrapper my-4 ms-4">
+                                <h3>Ingredients</h3>
+                                <ul id="list_ingredients"></ul>
+                            </div>
+                            <p id="instructions" class="px-4"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <span>Recipe By: <span id="nutritionist_name" class="text-secondary"></span></span>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Toast -->
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div class="toast align-items-center border-0" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+
+
     <?php include_once 'includes/footer.php'; ?>
     <?php include_once 'includes/external_js.php'; ?>
+
+    <script>
+        $(document).ready(function() {
+            // Toast init
+            const toastEl = document.querySelector('.toast');
+            const toast = new bootstrap.Toast(toastEl, {
+                autohide: true,
+            });
+            
+            $(".btn-recipe-details").on('click', function(e) {
+                e.preventDefault();
+                let id = $(this).data('id');
+                $.ajax({
+                    url: 'ajax/recipe_details.php',
+                    method: 'POST',
+                    data: {
+                        recipe_id: id
+                    },
+                    success: function(response) {
+                        let res = JSON.parse(response);
+                        $("#title").html(res.title);
+                        $("#list_ingredients").addClass('count_' + res.list_count).html(res.ingredients_list);
+                        $("#instructions").html(res.instructions);
+                        $("#nutritionist_name").html(res.nutritionist);
+                        $("#category_name").html(res.category_name);
+                    }
+                });
+            });
+
+
+            // Remove Recipe
+            $(".btn-recipe-fav").on("click", function(e) {
+                e.preventDefault();
+                let id = $(this).data("id");
+                let usrID = $(this).data("usr");
+                $.ajax({
+                    url: "ajax/delete.php",
+                    method: "POST",
+                    data: {
+                        fav_id: id,
+                        usrID: usrID
+                    },
+                    success: function(response) {
+                        let res = JSON.parse(response);
+                        $(".toast").addClass(res.class_);
+                        $(".toast-body").html(res.msg);
+                        toast.show();
+                        $(".btn-recipe-fav").css({
+                            'pointer-events': 'none',
+                            'background-color': '#777'
+                        });
+                        setTimeout(() => {
+                            window.location.reload();
+                        }, 1800);
+                    }
+                })
+            });
+        });
+    </script>
 </body>
 
 </html>
